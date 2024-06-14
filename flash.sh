c57a3b3ba42a7a2a41ebb238b5fe3e46388e0ff8
calint <calin.tenitchi@gmail.com>
2024-06-14 22:50:37 +0200

    .

--- flash.sh
+++ flash.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # write program
-openFPGALoader -b tangnano20k impl/pnr/riscv.fs
+# openFPGALoader -b tangnano20k impl/pnr/riscv.fs
 
 # write flash
-# openFPGALoader -b tangnano20k -f impl/pnr/riscv.fs
+openFPGALoader -b tangnano20k -f impl/pnr/riscv.fs
